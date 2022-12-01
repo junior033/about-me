9edd80e6ff9f41f8fa4fbf6e687853dcf700d8a0
Marlon Angulo Jr <marlonangulo001@gmail.com>
2022-11-30 19:50:32 -0500

    added lists to html and a counter and questions 6, 7 to js

--- js/app.js
+++ js/app.js
@@ -1,46 +1,85 @@
 'use strict';
 
+let answerTracker = 0;
 let userName = prompt('What is your name?');
 
 alert(`Welcome to my site ${userName}. Please answer the following quesitons with yes/no or y/n.`);
 
 let questionOneGuess = prompt('Do I have a son?').toLowerCase();
 
-if(questionOneGuess === 'yes' || questionOneGuess === 'y'){
-  alert('You got it right!');
-} else if(questionOneGuess === 'no' || questionOneGuess === 'n'){
+if (questionOneGuess === 'yes' || questionOneGuess === 'y') {
+  alert('You got it right!')
+  answerTracker++;
+} else if (questionOneGuess === 'no' || questionOneGuess === 'n') {
   alert('Sorry, you got it wrong.');
 }
 
 let questionTwoGuess = prompt('Do I live on a farm?').toLowerCase();
 
-if(questionTwoGuess === 'yes' || questionTwoGuess === 'y'){
+if (questionTwoGuess === 'yes' || questionTwoGuess === 'y') {
   alert('Sorry, you got it wrong');
-} else if(questionTwoGuess === 'no' || questionTwoGuess === 'n'){
+} else if (questionTwoGuess === 'no' || questionTwoGuess === 'n') {
   alert('You got it right!');
+  answerTracker++;
 }
 
 let questionThreeGuess = prompt('Was I in the military?').toLowerCase();
 
-if(questionThreeGuess === 'yes' || questionThreeGuess === 'y'){
+if (questionThreeGuess === 'yes' || questionThreeGuess === 'y') {
   alert('You got it right!');
-} else if(questionThreeGuess === 'no' || questionThreeGuess === 'n'){
+  answerTracker++;
+} else if (questionThreeGuess === 'no' || questionThreeGuess === 'n') {
   alert('Sorry, you got it wrong');
 }
 
 let questionFourGuess = prompt('Did I go to college?').toLowerCase();
 
-if(questionFourGuess === 'yes' || questionFourGuess === 'y'){
+if (questionFourGuess === 'yes' || questionFourGuess === 'y') {
   alert('You got it right!');
-} else if(questionFourGuess === 'no' || questionFourGuess === 'n'){
+  answerTracker++;
+} else if (questionFourGuess === 'no' || questionFourGuess === 'n') {
   alert('Sorry, you got it wrong');
 }
 
 let questionFiveGuess = prompt('Do I have a dog?').toLowerCase();
 
-if(questionFiveGuess === 'yes' || questionFiveGuess === 'y'){
-  alert(`Sorry, you got it wrong. Thanks for playing ${userName}!`);
-} else if(questionFiveGuess === 'no' || questionFiveGuess === 'n'){
-  alert(`You got it right! Thanks for playing ${userName}!`);
+if (questionFiveGuess === 'yes' || questionFiveGuess === 'y') {
+  alert('Sorry, you got it wrong.');
+} else if (questionFiveGuess === 'no' || questionFiveGuess === 'n') {
+  alert('You got it right!');
+  answerTracker++;
 }
 
+
+for (let i = 0; i < 4; i++) {
+  let questionSixGuess = prompt('How many countries have I visited?');
+
+  if (parseInt(questionSixGuess) === 16){
+    alert('You got it right!');
+    answerTracker++;
+    i = 4;
+  }else if (questionSixGuess > 16) {
+    alert('You guessed too high! Try again!');
+  } else if (questionSixGuess < 16) {
+    alert('You guessed too low! Try again!');
+
+  }
+}
+
+
+let favoriteFoods = ['pizza', 'pho', 'burritos', 'sticky-rice'];
+
+for (let i = 0; i < 6; i++){
+  let questionSevenGuess = prompt('What is one of my favorite meals to eat?');
+
+  if(questionSevenGuess === favoriteFoods[0] || questionSevenGuess === favoriteFoods[1] || questionSevenGuess === favoriteFoods[2] || questionSevenGuess === favoriteFoods[3]){
+    alert('You got it right!');
+    answerTracker++;
+    i = 6;
+  }else{
+    alert('You got it wrong! Try again!');
+  }
+}
+
+alert(`My favorite foods are: ${favoriteFoods}`);
+alert(`Thanks for playing ${userName}! You got ${answerTracker} out of 7 correct!`);
